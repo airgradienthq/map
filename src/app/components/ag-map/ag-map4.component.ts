7d57e2e36a0348fb946d3ea50881f4c4ae081a0a
arnavgupta <arnav@agupta.org>
2023-07-12 16:48:58 -0500

    eliminating redundant subscriptions and event listener setups

--- src/app/components/ag-map/ag-map4.component.ts
+++ src/app/components/ag-map/ag-map4.component.ts
@@ -1,5 +1,5 @@
-import {Component, ElementRef, NgZone, ViewChild} from '@angular/core';
-import {Map, Marker} from 'maplibre-gl';
+import {Component, ElementRef, NgZone, ViewChild, OnInit, AfterViewInit, OnDestroy} from '@angular/core';
+import {Map, Marker, NavigationControl} from 'maplibre-gl';
 import {DataServices} from "../../services/data.services";
 import {DetectDeviceServices} from "../../services/detect-device.services";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -11,6 +11,7 @@ import {Location} from "@angular/common";
 import {environment} from "../../../environments/environment.sample";
 import {UsAQIServices} from "../../services/usAQI.services";
 import {HttpClient} from "@angular/common/http";
+import { firstValueFrom } from 'rxjs';
 
 @Component({
 	selector: 'agMap4',
@@ -18,7 +19,7 @@ import {HttpClient} from "@angular/common/http";
 	templateUrl: 'ag-map4.component.html',
 })
 
-export class agMap4Component {
+export class agMap4Component implements OnInit, AfterViewInit, OnDestroy  {
 
 	private currentLongitude:number;
 	private currentLatitide:number;
@@ -29,9 +30,9 @@ export class agMap4Component {
 	// public selectedLocation: any = [];
 	autocompleteLocations: any;
 
-	map: Map | Map;
-	 @ViewChild('map')
-
+	map: Map | undefined;
+	 
+	@ViewChild('map')
 	private mapContainer!: ElementRef<HTMLElement>;
 
 	constructor(private Activatedroute: ActivatedRoute,
@@ -45,7 +46,111 @@ export class agMap4Component {
 				public detectDevice: DetectDeviceServices) {
 	}
 
+	ngOnInit() {	}
+
 	ngAfterViewInit() {
+		let params = this.Activatedroute.snapshot.queryParamMap;
+		console.log("dataServices.loadData 2");
+		this.currentZoom = +params.get('zoom') || 1;
+		this.currentLatitide = +params.get('lat') || 0;
+		this.currentLongitude = +params.get('long') || 0;
+		let token = params.get('token');
+		if (token != null) {
+			this.dataServices.token = token;
+			console.log("tkkk: " + this.dataServices.token)
+		}
+
+
+		const initialState = {
+			lng: this.currentLongitude,
+			lat: this.currentLatitide,
+			zoom: this.currentZoom
+		};
+
+
+		this.map = new Map({
+			container: this.mapContainer.nativeElement,
+			style: `https://api.maptiler.com/maps/streets-v2/style.json?key=vMpY3OLoCEkM7LpZcWdr`,
+			center: [initialState.lng, initialState.lat],
+			zoom: initialState.zoom
+		});
+
+		this.map.addControl(new NavigationControl({}), 'top-right');
+		
+		
+		this.map.on('mouseenter', 'locations', () => {
+			console.log("ss")
+			this.map.getCanvas().style.cursor = 'pointer'
+		})
+
+		this.map.on('mouseleave', 'locations', () => {
+			console.log("tt")
+			this.map.getCanvas().style.cursor = ''
+		})
+
+		let that = this;
+		this.map.on('moveend', function () {
+			console.log('Zoom. ' + Math.round(that.map.getZoom()));
+			console.log('Lat. ' + Math.round(that.map.getCenter().lat * 1000) / 1000);
+			console.log('Long. ' + Math.round(that.map.getCenter().lng * 1000) / 1000);
+
+			let zoom = Math.round(that.map.getZoom());
+			let lat = Math.round(that.map.getCenter().lat * 1000) / 1000;
+			let long = Math.round(that.map.getCenter().lng * 1000) / 1000
+			let org = that.currentOrgId;
+			let token = that.dataServices.token
+			let queryString = '?zoom=' + zoom + '&lat=' + lat + '&long=' + long + '&org=' + org + '&token=' + token;
+
+			that.location.replaceState("", queryString);
+		});
+
+		this.map.on('click', 'locations', function (e) {
+			const features = e.target.queryRenderedFeatures(e.point);
+			const locationsId = features[0].properties['sensor_nodes_id'];
+			console.log(features[0]);
+
+			var loc = new MapLocation()
+			loc.apiSource = "oaq";
+			loc.pm02 = features[0].properties['value'];
+			loc.locationId = locationsId;
+			that.dataServices.selectedLocation = loc;
+			that.bottomSheet.open(BottomSheetLocationComponent);
+		});
+
+		
+
+		this.map.on("load", () => {
+			this.map.addSource("locations", {
+				type: "vector",
+				tiles: [
+					"https://staging.openaq.org/v3/locations/tiles/{z}/{x}/{y}.pbf?parameters_id=2&active=true"
+				]
+			});
+			this.map.addLayer({
+				id: "locations",
+				type: "circle",
+				source: "locations",
+				"source-layer": "default",
+				"paint": {
+					"circle-radius": 12,
+					"circle-color": ["step",
+						['get', 'value'],
+						this.ColorService.getPM25Color(10),
+						12,
+						this.ColorService.getPM25Color(35),
+						35.4,
+						this.ColorService.getPM25Color(50),
+						55.4,
+						this.ColorService.getPM25Color(100),
+						150.4,
+						this.ColorService.getPM25Color(180),
+						250.4,
+						this.ColorService.getPM25Color(300)
+					]
+				},
+			});
+		});
+
 
 		this.Activatedroute.queryParamMap
 			.subscribe(params => {
@@ -59,7 +164,8 @@ export class agMap4Component {
 				//this.dataServices.loadDataOaq();
 
 			});
-
+		
+		this.loadDataAG();
 
 	}
 
@@ -73,119 +179,10 @@ export class agMap4Component {
 	}
 
 
-	ngOnInit() {
-		this.Activatedroute.queryParamMap
-			.subscribe(params => {
-				console.log("dataServices.loadData 2");
-				this.currentZoom = +params.get('zoom') || 1;
-				this.currentLatitide = +params.get('lat') || 0;
-				this.currentLongitude = +params.get('long') || 0;
-				let token = params.get('token');
-				if (token != null) {
-					this.dataServices.token = token;
-					console.log("tkkk: " + this.dataServices.token)
-				}
+	
 
-
-				const initialState = {
-					lng: this.currentLongitude,
-					lat: this.currentLatitide,
-					zoom: this.currentZoom
-				};
-
-
-				this.map = new Map({
-					container: this.mapContainer.nativeElement,
-					style: `https://api.maptiler.com/maps/streets-v2/style.json?key=vMpY3OLoCEkM7LpZcWdr`,
-					center: [initialState.lng, initialState.lat],
-					zoom: initialState.zoom
-				});
-
-				this.map.once("load", () => {
-					console.log("loaded")
-					this.map.addSource("locations", {
-						type: "vector",
-						tiles: [
-							"https://staging.openaq.org/v3/locations/tiles/{z}/{x}/{y}.pbf?parameters_id=2&active=true"
-						]
-					});
-
-
-					this.map.addLayer({
-						id: "locations",
-						type: "circle",
-						source: "locations",
-						"source-layer": "default",
-						"paint": {
-							"circle-radius": 12,
-							"circle-color": ["step",
-								['get', 'value'],
-								this.ColorService.getPM25Color(10),
-								12,
-								this.ColorService.getPM25Color(35),
-								35.4,
-								this.ColorService.getPM25Color(50),
-								55.4,
-								this.ColorService.getPM25Color(100),
-								150.4,
-								this.ColorService.getPM25Color(180),
-								250.4,
-								this.ColorService.getPM25Color(300)
-							]
-						},
-					});
-
-
-					var that = this
-
-					this.map.on('click', 'locations', function (e) {
-						const features = e.target.queryRenderedFeatures(e.point);
-						const locationsId = features[0].properties['sensor_nodes_id'];
-						console.log(features[0]);
-
-						var loc = new MapLocation()
-						loc.apiSource = "oaq";
-						loc.pm02 = features[0].properties['value'];
-						loc.locationId = locationsId;
-						that.dataServices.selectedLocation = loc;
-						that.bottomSheet.open(BottomSheetLocationComponent);
-					});
-
-					this.map.on('mouseenter', 'locations', () => {
-						console.log("ss")
-						this.map.getCanvas().style.cursor = 'pointer'
-					})
-
-					this.map.on('mouseleave', 'locations', () => {
-						console.log("tt")
-						this.map.getCanvas().style.cursor = ''
-					})
-
-					this.map.on('moveend', function () {
-						console.log('Zoom. ' + Math.round(that.map.getZoom()));
-						console.log('Lat. ' + Math.round(that.map.getCenter().lat * 1000) / 1000);
-						console.log('Long. ' + Math.round(that.map.getCenter().lng * 1000) / 1000);
-
-						let zoom = Math.round(that.map.getZoom());
-						let lat = Math.round(that.map.getCenter().lat * 1000) / 1000;
-						let long = Math.round(that.map.getCenter().lng * 1000) / 1000
-						let org = that.currentOrgId;
-						let token = that.dataServices.token
-						let queryString = '?zoom=' + zoom + '&lat=' + lat + '&long=' + long + '&org=' + org + '&token=' + token;
-
-						that.location.replaceState("", queryString);
-					});
-
-					this.loadDataAG();
-
-
-
-				})
-			});
-	}
-
-		loadDataAG(){
-		this.getAGRequest().subscribe((data: MapLocation[]) => {
+	async loadDataAG(){
+		await firstValueFrom(this.getAGRequest()).then((data: MapLocation[]) => {
 			this.agLocations = data;
 			this.agLocations.forEach( (location) => {
 				location.apiSource = 'ag';
